1d9088d9dfc4621f50592387f33cb6089746b1c1
LexusIgnatenko <lexus24081985@gmail.com>
2025-07-17 10:18:44 +0300

    init

---
+++ src/__tests__/basic.js
@@ -0,0 +1,81 @@
+//  test(description, work) 
+ //"description" - это описание что будет сделано
+ //"work" - какая-то функция, которая должна будет выполнена и где производится непосредственно сама проверка
+
+//  const { calculateTotal } = require("../calculate.js");
+
+//  test("basic test", () => {
+//      const result = 4;
+//      expect(result).toBe(4);
+//  });
+ 
+//  test("calculateTotal sum", () => {
+//      const list = [
+//          {
+//              id: 456,
+//              name: "Война и мир",
+//              count: 3,
+//              price: 400
+//          },
+//          {
+//              id: 77,
+//              name: "JavaScript",
+//              count: 1,
+//              price: 1300  
+//          }
+//      ];
+ 
+//      const result = calculateTotal(list);
+//      expect(result).toBe(2500);
+//  });
+ 
+ 
+ // Jest: import/export
+ 
+ 
+ import { calculateTotal } from "../calculate.js";
+ 
+ test("basic test", () => {
+     const result = 4;
+     expect(result).toBe(4);
+ });
+ 
+test("calculateTotal sum without discount", () => {
+     const list = [
+         {
+             id: 456,
+             name: "Война и мир",
+             count: 3,
+             price: 400
+         },
+         {
+             id: 77,
+             name: "JavaScript",
+             count: 1,
+             price: 1300
+         }
+     ];
+ 
+     const result = calculateTotal(list);
+     expect(result).toBe(2500);
+ });
+
+test("calculateTotal sum with discount", () => {
+    const list = [
+        {
+            id: 456,
+            name: "Война и мир",
+            count: 3,
+            price: 400
+        },
+        {
+            id: 77,
+            name: "JavaScript",
+            count: 1,
+            price: 1300
+        }
+    ];
+
+    const result = calculateTotal(list, true);
+    expect(result).toBe(1250);
+});
---
+++ src/calculate.js
@@ -0,0 +1,42 @@
+// import { of, reduce } from "core-js/core/array";
+
+// function calculateTotal(purchases) {
+//     let result = 0;
+//     for (let i = 0; i < purchases.length; i++) {
+//         result += purchases[i].count * purchases[i].price;
+//     }
+//     return result;
+// }
+
+// module.exports = {
+//     calculateTotal
+// }
+
+// Jest: import/export
+
+// export function calculateTotal(purchases) {
+//     let result = 0;
+//     for (let i = 0; i < purchases.length; i++) {
+//         result += purchases[i].count * purchases[i].price;
+//     }
+//     return result;
+// }
+
+// Использование метода массива reduce вместо цикла for...of
+
+// export function calculateTotal(purchases) {
+//     return purchases.reduce((total, item) => {
+//         return total + (item.count * item.price);
+//     }, 0);
+// }
+
+export function calculateTotal(purchases, applyDiscount) {
+    const sum = purchases.reduce((total, item) => {
+        return total + (item.count * item.price);
+    }, 0);
+
+    if (applyDiscount) {
+        return sum * 0.5;
+    }
+    return sum;
+}
