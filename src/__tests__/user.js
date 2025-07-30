f1258259aa29d42042eeec54ddc914462f83280c
LexusIgnatenko <lexus24081985@gmail.com>
2025-07-30 09:38:58 +0300

    update eslint

--- src/__tests__/user.js
+++ src/__tests__/user.js
@@ -8,5 +8,5 @@ beforeEach(() => {
 test("should call loadUser once", () => {
     httpGet.mockReturnValue(JSON.stringify({}));
     loadUser(1);
-    expect(httpGet).toHaveBeenLastCalledWith("http://server:8080/users/1");
+    expect(httpGet).toHaveBeenLastCalledWith("http://server:8080/users/1")
 });
